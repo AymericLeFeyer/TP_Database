b73e68e13fe2fed89f2ace5a2893ae2d48a30975
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-03-16 11:06:55 +0100

    Trigger 1 et debut 2 Postgres

--- PostGreSQL/triggers.sql
+++ PostGreSQL/triggers.sql
@@ -1,17 +1,63 @@
-create or replace function nomTrigger()
-returns trigger as $nomTrigger$ declare nom_var integer ;
+drop trigger trigger1 on projet;
+drop trigger trigger2 on projet;
+
+-- Affiche un message a la creation d'un projet
+
+create or replace function trigger1()
+returns trigger as $trigger1$ declare nom_var integer ;
     begin
-    raise notice 'Coucou';
+    raise notice 'Un nouveau projet est créé';
     return new;
 
 
     end;
 
-    $nomTrigger$ language plpgsql;
+    $trigger1$ language plpgsql;
 
-create trigger nomTrigger after insert on client
-    execute procedure nomTrigger();
+create trigger trigger1 after insert on projet
+    execute procedure trigger1();
 
-drop trigger nomTrigger on client;
 
--- Faire un trigger qui, quand on ajoute un projet, ajoute le montant du projet au montant total du chantier (ou l'inverse je sais plus en fait, au secours conrad)
+
+-- Lorsqu'un projet est cree, le montant des travaux est init a 0, Ce montant des travaux sera calcule a partir des budgets dedies
+-- a chaque chantier - mis a jour a chaque insertion
+
+create or replace function trigger2()
+returns trigger as $trigger2$ declare nom_var integer ;
+    begin
+    raise notice 'Montant init a 0';
+    new.montant := 0;
+    return new;
+
+
+    end;
+
+    $trigger2$ language plpgsql;
+
+create trigger trigger3 before insert on projet
+    for each row
+    execute procedure trigger3();
+
+create or replace function trigger3()
+returns trigger as $trigger3$
+    declare a double precision;
+
+    begin
+    raise notice 'Update projet montant';
+
+    update projet
+    set projet.montant = projet.montant + new.budget
+    where new.idprojet = projet.idprojet;
+
+
+    return new;
+
+
+    end;
+
+    $trigger3$ language plpgsql;
+
+create trigger trigger3 before insert on chantier
+    for each row
+    execute procedure trigger3();
+
