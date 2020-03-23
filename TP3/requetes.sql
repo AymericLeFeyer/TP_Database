47e01aa54caf160bb68fac3b720fb047722d28c4
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-03-23 14:57:04 +0100

    requetes

--- TP3/requetes.sql
+++ TP3/requetes.sql
@@ -5,9 +5,3 @@ where enfant.prenom = 'Conrad';
 select count(organisation)
 from categorie, organisation, activite
 where organisation.id_activite = activite.id_activite;
-
-select organisation.creneauhoraire
-from organisation
-where organisation.id_enfant = 1
-and organisation.id_journee = 1;
-
